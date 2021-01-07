4500cb677a9c8b83fc20b9e396d4cdb60480a4aa
Edgar Melo <edgarmelo@acad.ifma.edu.br>
2021-01-07 15:32:53 -0300

    Add Footer

---
+++ front-web/src/Footer/index.tsx
@@ -0,0 +1,27 @@
+import './styles.css';
+
+import {ReactComponent as Logo } from './logo.svg';
+import {ReactComponent as YoutubeIcon } from './youtube.svg';
+import {ReactComponent as InstagramIcon } from './instagram.svg';
+import { ReactComponent as LinkedinIcon} from './linkedin.svg';
+
+function Footer(){
+    return (
+        <footer className="main-footer">
+                App desenvolvido para Delivery -- [V-2021.00]
+                <div className="footer-icons">
+                    <a href="https://www.youtube.com/channel/UCfV2GinYcbS0va54uCCirKw" target="_new">
+                        <YoutubeIcon/>
+                    </a>
+                    <a href="https://www.linkedin.com/in/edgar-costa-melo-73b384203/" target="_new">
+                        <LinkedinIcon/>
+                    </a>
+                    <a href="https://www.instagram.com/ecmedgar/" >
+                        <InstagramIcon/>
+                    </a>
+                </div>
+        </footer>
+    )
+}
+
+export default Footer;
